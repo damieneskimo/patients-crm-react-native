e1b3cf0423ef3ec94b5b48d48f320eb6a439fd62
Damien Jiang <damieneskimo@gmail.com>
2021-05-06 23:29:02 +1200

    load custom fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
 import Routes from './src/Routes';
 import { AuthProvider } from './src/AuthProvider';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+}
 
 export default function App() {
+  const [ isReady, setIsReady ] = useState(false);
+
+  if (! isReady) {
+    return (
+      <AppLoading 
+        startAsync={fetchFonts}
+        onFinish={() => setIsReady(true)}
+        onError={console.warn}
+      />
+    )
+  }
+  
   return (
       <AuthProvider>
         <Routes />
