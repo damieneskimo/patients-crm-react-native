b30c5d08522d9986aa6d3fae7510a2ea2921b874
Damien Jiang <damieneskimo@gmail.com>
2021-05-06 23:30:49 +1200

    style login screen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { AuthContext } from "../AuthProvider";
 
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../types';
+import Colors from '../constants/Colors';
+import ScreenText from '../components/ScreenText';
 
 type LoginScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -20,29 +22,50 @@ const LoginScreen = ({ navigation }: Props) => {
     const [password, setPassword] = useState('');
   
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container}>
+        <ScreenText style={ styles.heading }>Patients CRM</ScreenText>
         { error &&
-          <Text style={{ color: 'red', marginBottom: 24 }}>{ error }</Text>
+          <ScreenText style={styles.error}>{ error }</ScreenText>
         }
         <TextInput
-          style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1, padding: 8 }}
+          style={styles.input}
           onChangeText={text => setEmail(text)}
           placeholder="Email"
           textContentType="emailAddress"
           autoCapitalize = 'none'
         />
         <TextInput
-          style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1, padding: 8, marginTop: 24 }}
+          style={[styles.input, { marginTop: 24 } ]}
           onChangeText={text => setPassword(text)}
           placeholder="Password"
           secureTextEntry={true}
         />
-        <Button
-          title="Login"
-          onPress={() => login(email, password)}
-        />
+        <View style={styles.actions}>
+            <Button
+            title="Login"
+            onPress={() => login(email, password)}
+            />
+        </View>
       </View>
     );
   }
+
+  const styles = StyleSheet.create({
+    container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+    heading: {
+        fontSize: 25,
+        color: Colors.primary,
+        marginBottom: 20
+    },
+    error: { color: 'red', marginBottom: 24 },
+    input: { height: 40, width: 300, borderColor: 'gray', borderWidth: 1, padding: 8 },
+    actions: {
+        marginTop: 20,
+        paddingVertical: 8,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+        backgroundColor: Colors.primary
+    }
+  })
 
   export default LoginScreen;
